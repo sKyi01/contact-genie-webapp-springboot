0a6307309d4bf48bfc92bc23bdaa2a8296f90d9a
sKyi01 <113761204+sKyi01@users.noreply.github.com>
2023-06-26 12:58:25 +0530

    "updated 1"

--- ContactGenie/src/main/java/com/spring/genie/controller/UserController.java
+++ ContactGenie/src/main/java/com/spring/genie/controller/UserController.java
@@ -33,6 +33,7 @@ public class UserController {
 
 	@ModelAttribute
 	public void handleCommonRequest(Model m, Principal p) {
+		System.out.println("intellij idea");
 
 		String username = p.getName();
 		System.out.println("USER " + username);
